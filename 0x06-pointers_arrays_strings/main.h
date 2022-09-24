9ec94ef919bb7a801fb9d8ac4f9e907a0f2582b1
Kwaghfan <whizkwghfan94@gmail.com>
2022-09-23 21:51:14 -0700

    Task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -7,4 +10,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-char _putchar(char c);
+char *rot13(char *);
+
+#endif
