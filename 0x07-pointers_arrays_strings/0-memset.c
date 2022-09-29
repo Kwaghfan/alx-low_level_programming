d0a056dd11bdfbd46ec80f75e6da7406cfbe20e7
Kwaghfan <whizkwghfan94@gmail.com>
2022-09-29 16:59:19 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,14 +6,14 @@
  * @n: bytes of the memory
  * Return: dest
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	/* Declaring FOR */
+	/*Declaring FOR*/
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b; /*add 1 position s*/
-	} /* END FOR */
+		*(s + 1) = b; /*add 1 position s*/
+	} /*END FOR*/
 	return (s);
 }
