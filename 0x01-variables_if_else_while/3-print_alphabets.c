657a77ee9fe609551a537d1d5b253738bda99732
Kwaghfan <whizkwghfan94@gmail.com>
2022-10-25 07:48:36 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print a-z and A-Z using lowercase and uppercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; ++n)
+	{
+		putchar(n);
+	}
+	for (n = 'A'; n <= 'Z'; ++n)
+	{
+		putchar(n);
+	}
+
+	putchar (10);
+	return (0);
+}
